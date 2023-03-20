9c88ef60aaed86980eecbaebfa199772df39c5fb
Hugo Ramon <ramone.techie@gmail.com>
2023-03-20 13:45:31 -0300

    created sidebar and its styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { Header } from './components/header/Header';
-import { Post } from './components/post/Post';
+import { Header } from './components/Header/Header';
+import { Post } from './components/Post/Post';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Post
-        author="Hugo Ramon"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, doloribus"
-      />
-      <Post
-        author="Higor Diego"
-        content="A very experienced Software Developer who now is working as an Architect of Solutions"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Hugo Ramon"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, doloribus"
+          />
+          <Post
+            author="Higor Diego"
+            content="A very experienced Software Developer who now is working as an Architect of Solutions"
+          />
+        </main>
+      </div>
     </>
   );
 };
