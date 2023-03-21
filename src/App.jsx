9ab6db7762533697d340765b25b4ced31b2ca6e6
Hugo Ramon <ramone.techie@gmail.com>
2023-03-20 21:03:22 -0300

    Finished sidebar and content

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ export const App = () => {
   return (
     <>
       <Header />
-
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import styles from './Post.module.css';
 
-export const Post = (props) => {
+export const Post = () => {
   return (
-    <>
-      <strong>{props.author}</strong>
-      <p>{props.content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/HugoRamonPereira.png" />
+          <div className={styles.authorInfo}>
+            <p>Hugo Ramon</p>
+            <span>Frontend Developer</span>
+          </div>
+        </div>
+
+        <time
+          title='March, 20th at 02:19h'
+          dateTime='2023-03-20 02:19:45'
+        >
+          Published 1h ago
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Hello guys 👋🏼</p>
+        <p>Just finished a new project, and it is pretty nice, you guys can check it at.</p>
+        <p>{' '}<a href="">ramon/alienlabs</a></p>
+        <p>
+          <a href="">#newproject </a>{' '}
+          <a href="">#frontend </a>{' '}
+          <a href="">#webdevelopent</a>{' '}
+        </p>
+      </div>
+    </article>
   );
 };
